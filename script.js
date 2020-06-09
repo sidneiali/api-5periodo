9cdcaa1be366846d289d236c693d6ebdfe6e6421
Sidnei Ali <sidneiali01@gmail.com>
2020-06-08 21:17:55 -0300

    feat: insert edge

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const radius = 20;
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-const data = [];
+const dataCyrcle = [];
+const dataLine = [];
 
 var context;
-var count = 0;
+var countCyrcle = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
   const letter = alphabet.split('');
@@ -33,12 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     generateCyrcle(context, clientClick.x, clientClick.y, radius, letter);
 
-    console.log(data);
+    console.log(dataCyrcle);
   }, true);
 });
 
 const generateCyrcle = (context, positionX, positionY, radiusCyrcle, letter) => {
-  if (!letter[count]) {
+  if (!letter[countCyrcle]) {
     alert('Fim de caracteres');
     return;
   }
@@ -46,22 +47,45 @@ const generateCyrcle = (context, positionX, positionY, radiusCyrcle, letter) => 
   context.beginPath();
   context.imageSmoothingEnabled = true;
   context.arc(positionX - radiusCyrcle, positionY - radiusCyrcle, radiusCyrcle, 0, 2  * Math.PI);
-  letter && context.strokeText(letter[count], positionX - radiusCyrcle, positionY - radiusCyrcle);
+  letter && context.strokeText(letter[countCyrcle], positionX - radiusCyrcle, positionY - radiusCyrcle);
   context.stroke();
 
-  const dataCyrcle = {
-    letter: letter[count],
+  const objectCyrcle = {
+    letter: letter[countCyrcle],
     x: positionX,
-    y: positionX,
+    y: positionY,
   }
 
-  data.push(dataCyrcle);
+  dataCyrcle.push(objectCyrcle);
 
-  count++;
+  countCyrcle++;
 };
+
+const generateLine = (context, startingPosition, finalPosition) => {
+  const initialIndex = dataCyrcle.findIndex(cyrcle => 
+    cyrcle.letter === startingPosition);
+
+  const finalIndex = dataCyrcle.findIndex(cyrcle => 
+    cyrcle.letter === finalPosition);
+
+  if ( initialIndex >= 0 && finalIndex >= 0 && initialIndex !== finalIndex ) {
+    context.beginPath();
+    context.moveTo(dataCyrcle[initialIndex].x - radius, dataCyrcle[initialIndex].y - radius);
+    context.lineTo(dataCyrcle[finalIndex].x - radius, dataCyrcle[finalIndex].y - radius);
+    context.stroke();
+
+    const objectLine = {
+      start: dataCyrcle[initialIndex].letter, 
+      final: dataCyrcle[finalIndex].letter,
+    }
+  
+    dataLine.push(objectLine);
+  }
+  console.log(dataLine);
+}
 
 const clearCanvas = () => {
   context.clearRect(0, 0, screen.width, screen.height);
-  data.length = 0;
-  count = 0;
+  dataCyrcle.length = 0;
+  countCyrcle = 0;
 }
